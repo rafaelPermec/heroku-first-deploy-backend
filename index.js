f7b82dc360dab2747ef4352793eb67b7e7d85881
rafaelPermec <geekchef.dev@gmail.com>
2022-07-05 12:04:06 -0300

    :construction: feat: deploy de backend com Heroku e Docker

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
+const xablau = `
+<h1>Contrate o Rafael Perdigão agora mesmo!</h1>
+  <p>Seu e-mail é dev.rafaelpermec@gmail.com</p><br>
+  <h2>Estamos ouvindo sua porta ${port}, viu?</h2>`;
+
+app.get('/', (req, res) => res.send(xablau));
 app.listen(port, () => console.log(`Online: ${port}`));
